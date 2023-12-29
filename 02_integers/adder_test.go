e9c529a31a68a5889495627d83e21aac3d08e40c
Dheerapat Tookkane <dheerapat38@gmail.com>
2023-12-29 15:54:59 +0700

    chapter 2

---
+++ 02_integers/adder_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(4, 2)
+	expected := 6
+
+	if sum != expected {
+		t.Errorf("got %d expected %d", sum, expected)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
